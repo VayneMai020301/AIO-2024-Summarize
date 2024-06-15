774a3003939fa69cb81d653bee8707b9a1158091
DEV <maitrungkien1296@gmail.com>
2024-06-15 14:42:31 +0700

    summarization week1

--- Week1_Calculus_Application/circle_area.py
+++ Week1_Calculus_Application/circle_area.py
@@ -10,15 +10,13 @@ def fx(x) -> float :
 
 def run(n_sample):
     area = 0
-    start = -1 
-    stop = 1
+
     step =   2 / n_sample
+
     for i in range(n_sample):
         area+= step * fx(-1 + i *step)
 
-
     return 2* area
-
 
 if __name__ == "__main__":
     print(f"Area of Circle is: {run(20000)}")
